98ee7fb4fd5db4c1286b3a71570bdcaf108c8737
ManishaDhage <115449875+ManishaDhage@users.noreply.github.com>
2023-03-21 18:29:57 +0530

    Update streamlit_app.py
    option added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale and Green Smoothie')
-streamlit.text('Hard-boiled free range egg')
+streamlit.text(' 🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale and Green Smoothie')
+streamlit.text('🐔 Hard-boiled free range egg')
+streamlit.text('🥑🍞 Avacado Toast')
